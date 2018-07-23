5733eb9c25cfe1de30cf1b38fd18134228038ab3
kaursandeep96 <sandeepkaur@dal.ca>
2018-07-23 00:25:28 -0300

    visulaized results

--- best.py
+++ best.py
@@ -1,10 +1,16 @@
+import pandas as pd
+import numpy as np 
+import nltk
+from functools import partial 
+aisles_data = pd.read_csv('instacart/aisles.csv')
+print('Total no. of aisles: {}'.format(aisles_data.shape[0]))
+aisles_data
+departments_data = pd.read_csv('instacart/departments.csv')
 %%time
-# popular products (total set, not only train)
 top = 15
 top_products = pd.merge(
-    # to see train: 
-    # left=pd.DataFrame(order_details[order_details.eval_set == 'train'].groupby(['product_id'])['order_id']\
     left=pd.DataFrame(order_details.groupby(['product_id'])['order_id']\
     .apply(lambda x: len(x.unique())).sort_values(ascending=False)[:top].reset_index('product_id')),
     right=goods,
     how='left')
+top_products
